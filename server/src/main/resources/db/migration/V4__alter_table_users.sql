99b5dbb11fd37cc2977b807ebf0e1d479370ffe7
Mete G <mete.guneysel@gmail.com>
2024-06-26 10:16:57 +0300

    Drop the "schema" idea

--- server/src/main/resources/db/migration/V4__alter_table_users.sql
+++ server/src/main/resources/db/migration/V4__alter_table_users.sql
@@ -1,8 +1,8 @@
 -- Rename the username column to name
-ALTER TABLE ${schema}.users RENAME COLUMN username TO name;
+ALTER TABLE weshare.users RENAME COLUMN username TO name;
 
 -- Add the group_id column to users table
-ALTER TABLE ${schema}.users ADD COLUMN group_id UUID;
+ALTER TABLE weshare.users ADD COLUMN group_id UUID;
 
 -- Enable the uuid-ossp extension if not already enabled
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
@@ -13,12 +13,12 @@ DO $$
         group_uuid UUID := uuid_generate_v4();
     BEGIN
         -- Populate the group_id column with the same UUID for the two existing users
-        UPDATE ${schema}.users SET group_id = group_uuid;
+        UPDATE weshare.users SET group_id = group_uuid;
     END $$;
 
 -- Ensure group_id column is not null
-ALTER TABLE ${schema}.users ALTER COLUMN group_id SET NOT NULL;
+ALTER TABLE weshare.users ALTER COLUMN group_id SET NOT NULL;
 
 -- Add a unique constraint on the combination of name and group_id
-ALTER TABLE ${schema}.users
+ALTER TABLE weshare.users
     ADD CONSTRAINT unique_name_group_id UNIQUE (name, group_id);
--- server/src/main/resources/db/migration/V5__alter_bills_table_to_use_user_id.sql
+++ server/src/main/resources/db/migration/V5__alter_bills_table_to_use_user_id.sql
@@ -1,22 +1,22 @@
 -- Step 1: Drop the existing fk_owner constraint if it exists
-ALTER TABLE ${schema}.bills DROP CONSTRAINT IF EXISTS fk_owner;
+ALTER TABLE weshare.bills DROP CONSTRAINT IF EXISTS fk_owner;
 
 -- Step 2: Add a temporary column for the new owner_id
-ALTER TABLE ${schema}.bills ADD COLUMN owner_id INTEGER;
+ALTER TABLE weshare.bills ADD COLUMN owner_id INTEGER;
 
 -- Step 3: Populate the temporary column based on the current owner (username)
-UPDATE ${schema}.bills b
+UPDATE weshare.bills b
 SET owner_id = (
     SELECT u.id
-    FROM ${schema}.users u
+    FROM weshare.users u
     WHERE u.name = b.owner
 );
 
 -- Step 4: Drop the old owner column
-ALTER TABLE ${schema}.bills DROP COLUMN owner;
+ALTER TABLE weshare.bills DROP COLUMN owner;
 
 -- Step 5: Rename the temporary owner_id column to owner
-ALTER TABLE ${schema}.bills RENAME COLUMN owner_id TO owner;
+ALTER TABLE weshare.bills RENAME COLUMN owner_id TO owner;
 
 -- Step 6: Add foreign key constraint to ensure referential integrity with the users table
-ALTER TABLE ${schema}.bills ADD CONSTRAINT fk_owner FOREIGN KEY (owner) REFERENCES ${schema}.users (id);
+ALTER TABLE weshare.bills ADD CONSTRAINT fk_owner FOREIGN KEY (owner) REFERENCES weshare.users (id);
