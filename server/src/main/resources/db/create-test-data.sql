b835f91bb048482b01a9bb82fbf536716373cc13
Mete G <mete.guneysel@gmail.com>
2024-06-26 10:17:08 +0300

    Fix test data

--- server/src/main/resources/db/create-test-data.sql
+++ server/src/main/resources/db/create-test-data.sql
@@ -1,23 +1,22 @@
 BEGIN;
 
+-- Insert initial user records with the same group_id and ensure ids start from 1
 DO $$
-DECLARE
-    group_uuid UUID := uuid_generate_v4();
-BEGIN
-    -- Insert initial user records with the same group_id
-    INSERT INTO weshare.users (name, password, role, group_id)
-    VALUES
-        ('user', '$2a$10$aBNsZVVWtDI0ZxcYue/30ebE0qsL7qT49uhxEvU1xJ3lp9GHVgSD6', 'Role1', group_uuid),
-        ('user2', '$2a$10$uM20SAvEWY9X1b5wVEoZ3uBv0Xr8ucHatGZgGowEJ9ETWwZZWseaW', 'Role2', group_uuid);
-END $$;
+    DECLARE
+        group_uuid UUID := uuid_generate_v4();
+    BEGIN
+        -- Insert initial user records with the same group_id
+        INSERT INTO weshare.users (id, name, password, role, group_id)
+        VALUES
+            (1, 'user', '$2a$10$aBNsZVVWtDI0ZxcYue/30ebE0qsL7qT49uhxEvU1xJ3lp9GHVgSD6', 'Role1', group_uuid),
+            (2, 'user2', '$2a$10$uM20SAvEWY9X1b5wVEoZ3uBv0Xr8ucHatGZgGowEJ9ETWwZZWseaW', 'Role2', group_uuid);
+    END $$;
 
 -- Generate 1000 bills between last year and this year with random owners
-DO
-$$
+DO $$
     DECLARE
-        ownerNames   TEXT[] := ARRAY ['user', 'user2'];
         descriptions TEXT[] := ARRAY ['Food', 'Gasoline', 'Food for cats', 'Electric bill', 'Water bill', 'Walmart'];
-        owner        TEXT;
+        owner        INTEGER;
         amount       NUMERIC;
         description  TEXT;
         category     INTEGER;
@@ -27,9 +26,9 @@ $$
         i            INT;
         percentage   INTEGER;
     BEGIN
-        FOR i IN 1..1000
-            LOOP
-                owner := ownerNames[1 + (i % array_length(ownerNames, 1))];
+        FOR i IN 1..1000 LOOP
+                -- Randomly select owner (1 or 2)
+                owner := 1 + floor(random() * 2)::int;
 
                 -- Generate amount in cents (range 2000 to 10000 cents)
                 amount := (20 + (floor(random() * 9) * 10)) * 100;
@@ -49,7 +48,6 @@ $$
                 INSERT INTO weshare.bills (owner, amount, description, category, date, own_amount, is_paid)
                 VALUES (owner, amount, description, category, date, own_amount, is_paid);
             END LOOP;
-    END
-$$;
+    END $$;
 
 COMMIT;
