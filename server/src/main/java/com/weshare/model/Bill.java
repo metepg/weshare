06eb3ece71370fd0bff31a6bca33d3800deee3a6
Mete G <mete.guneysel@gmail.com>
2024-06-27 15:25:26 +0300

    Add lombok

--- server/src/main/java/com/weshare/model/Bill.java
+++ server/src/main/java/com/weshare/model/Bill.java
@@ -2,7 +2,6 @@ package com.weshare.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.weshare.util.MoneyConverter;
-
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,11 +10,19 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "bills", schema = "weshare")
 public class Bill implements Serializable {
@@ -36,90 +43,14 @@ public class Bill implements Serializable {
     @JsonIgnore
     private Group group;
 
-    private String description;
+    @Convert(converter = MoneyConverter.class)
+    private double ownAmount;
 
     @Convert(converter = MoneyConverter.class)
     private double amount;
 
     private int category;
-
     private LocalDate date;
-
-    @Convert(converter = MoneyConverter.class)
-    private double ownAmount;
-
     private boolean paid;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public User getOwner() {
-        return owner;
-    }
-
-    public void setOwner(User owner) {
-        this.owner = owner;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-    public int getCategory() {
-        return category;
-    }
-
-    public void setCategory(int category) {
-        this.category = category;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public double getOwnAmount() {
-        return ownAmount;
-    }
-
-    public void setOwnAmount(double ownAmount) {
-        this.ownAmount = ownAmount;
-    }
-
-    public boolean isPaid() {
-        return paid;
-    }
-
-    public void setPaid(boolean paid) {
-        this.paid = paid;
-    }
-
-    public Group getGroup() {
-        return group;
-    }
-
-    public void setGroup(Group group) {
-        this.group = group;
-    }
-
+    private String description;
 }
--- server/src/main/java/com/weshare/model/Group.java
+++ server/src/main/java/com/weshare/model/Group.java
@@ -8,23 +8,30 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.List;
 import java.util.UUID;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "groups", schema = "weshare")
 public class Group implements Serializable {
+
     @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
-
-    private String name;
 
     @OneToMany(mappedBy = "group")
     @JsonManagedReference
@@ -34,36 +41,5 @@ public class Group implements Serializable {
     @JsonIgnore
     private List<Bill> bills;
 
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Bill> getBills() {
-        return bills;
-    }
-
-    public void setBills(List<Bill> bills) {
-        this.bills = bills;
-    }
-
-    public List<User> getUsers() {
-        return users;
-    }
-
-    public void setUsers(List<User> users) {
-        this.users = users;
-    }
-
+    private String name;
 }
