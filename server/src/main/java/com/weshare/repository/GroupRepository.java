b7d61306cd5e331ba5b22790453066670f8402eb
Mete G <mete.guneysel@gmail.com>
2024-06-28 12:42:22 +0300

    Modify constraint
    - User can exist without group.

---
+++ server/src/main/java/com/weshare/repository/GroupRepository.java
@@ -0,0 +1,10 @@
+package com.weshare.repository;
+
+import com.weshare.model.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface GroupRepository extends JpaRepository<Group, UUID> {
+
+}
