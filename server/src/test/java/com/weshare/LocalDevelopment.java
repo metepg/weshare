0ef726e779895064ee26ade2134e3dfac8d02acc
metepg <mete.guneysel@gmail.com>
2024-12-30 13:29:41 +0200

    Add Testcontainers initialization for local development
    - App can be started locally by running the LocalDevelopment main method with profile local.

---
+++ server/src/test/java/com/weshare/LocalDevelopment.java
@@ -0,0 +1,21 @@
+package com.weshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public class LocalDevelopment {
+
+    public static void main(String[] args) {
+        SpringApplication.from(Application::main).with(LocalDevelopment.class).run(args);
+    }
+
+    @Bean
+    @ServiceConnection
+    public PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>("postgres:16-alpine")
+                .withDatabaseName("weshare");
+    }
+
+}
