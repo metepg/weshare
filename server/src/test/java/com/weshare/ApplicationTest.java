4383ea98ac480f9055d4ad2b7ce5d735b644296c
metepg <mete.guneysel@gmail.com>
2024-12-30 13:36:03 +0200

    Add simple test

---
+++ server/src/test/java/com/weshare/ApplicationTest.java
@@ -0,0 +1,24 @@
+package com.weshare;
+
+import com.weshare.service.BillService;
+import com.weshare.service.CategoryService;
+import com.weshare.service.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ApplicationTest extends AbstractApplicationTest {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @DisplayName("Services exist")
+    @Test
+    void applicationContextExist() {
+        assertThat(applicationContext.getBean(BillService.class)).isNotNull();
+        assertThat(applicationContext.getBean(CategoryService.class)).isNotNull();
+        assertThat(applicationContext.getBean(UserService.class)).isNotNull();
+    }
+}
