61433a78575134fc918530cb0f73113565c580e1
metepg <mete.guneysel@gmail.com>
2025-01-04 22:22:57 +0200

    Fix migrations

--- server/src/main/resources/db/migration/V1__create_initial_tables.sql
+++ server/src/main/resources/db/migration/V1__create_initial_tables.sql
@@ -1,28 +1,24 @@
 CREATE SCHEMA weshare;
 
--- Enable the uuid-ossp extension if not already enabled
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- Create the groups table
 CREATE TABLE weshare.groups
 (
-    created_at TIMESTAMP        DEFAULT CURRENT_TIMESTAMP,
     id         UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP        DEFAULT CURRENT_TIMESTAMP,
     name       TEXT
 );
 
--- Create the users table
 CREATE TABLE weshare.users
 (
-    group_id UUID,
     id       SERIAL PRIMARY KEY,
+    group_id UUID,
     name     TEXT UNIQUE,
     password TEXT,
     role     TEXT,
     CONSTRAINT unique_name_group_id UNIQUE (id, group_id)
 );
 
--- Create the categories table
 CREATE TABLE weshare.categories
 (
     id       SERIAL PRIMARY KEY,
@@ -31,18 +27,15 @@ CREATE TABLE weshare.categories
     CONSTRAINT fk_group FOREIGN KEY (group_id) REFERENCES weshare.groups (id)
 );
 
--- Create the bills table
 CREATE TABLE weshare.bills
 (
+    id          SERIAL PRIMARY KEY,
     amount      INTEGER CHECK (amount <= 999999), -- Max amount 9999,99
     category    SMALLINT CONSTRAINT fk_category REFERENCES weshare.categories (id),
     date        DATE,
     description TEXT,
-    id          SERIAL PRIMARY KEY,
     paid        BOOLEAN,
-    own_amount  INTEGER
-        CHECK (amount <= 999999), -- Max amount 9999,99
+    own_amount  INTEGER CHECK (amount <= 999999), -- Max amount 9999,99
     owner       INTEGER
         CONSTRAINT fk_owner REFERENCES weshare.users (id)
 );
-
