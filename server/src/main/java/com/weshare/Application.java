f7fa0364957fff843b054792034bdf639d61d6b8
metepg <mete.guneysel@gmail.com>
2025-09-16 11:14:43 +0300

    Remove Flyway repair fix

--- server/src/main/java/com/weshare/Application.java
+++ server/src/main/java/com/weshare/Application.java
@@ -2,8 +2,6 @@ package com.weshare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.flyway.autoconfigure.FlywayMigrationStrategy;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -12,11 +10,4 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-    @Bean
-    public FlywayMigrationStrategy repairThenMigrate() {
-        return flyway -> {
-            flyway.repair();
-            flyway.migrate();
-        };
-    }
 }
