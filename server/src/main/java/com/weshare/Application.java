19dc4df1c6d5e71852453dfbc8c9c6e963368a49
metepg <mete.guneysel@gmail.com>
2025-09-16 10:56:48 +0300

    Use Flyway repair to sync database migrations

--- server/src/main/java/com/weshare/Application.java
+++ server/src/main/java/com/weshare/Application.java
@@ -2,6 +2,8 @@ package com.weshare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.flyway.autoconfigure.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +12,11 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+    @Bean
+    public FlywayMigrationStrategy repairThenMigrate() {
+        return flyway -> {
+            flyway.repair();
+            flyway.migrate();
+        };
+    }
 }
